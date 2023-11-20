49b48f16620d29a132714c8f6a8d85c6aede7fe1
AnmolBennett <131250507+AnmolBennett@users.noreply.github.com>
2023-11-20 18:58:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_Fd40UdDlz", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "6eost5lk6vap71iianc9vua2r5", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
