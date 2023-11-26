e668c4ae8b5caddddf1d9f8eee1d261341541a34
AnmolBennett <131250507+AnmolBennett@users.noreply.github.com>
2023-11-26 12:49:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_w6ntbKpJ4", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "7fjsn60klq3shrkmtu85j46bs6", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-east-1", // e.g. us-east-2
+    userPoolId: "", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://38psvinu5c.execute-api.us-east-1.amazonaws.com/production", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
